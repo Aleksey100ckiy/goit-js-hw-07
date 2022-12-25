812983bef2b23c80f16a3e30b1363ea23e53fb1b
Aleksey100ckiy <109811250+Aleksey100ckiy@users.noreply.github.com>
2022-12-26 00:22:37 +0200

    try make 7HW

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,34 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector('.gallery');
+
+
+
+const images = galleryItems.reduce((acc,{preview, original, description}) => acc +`
+<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</div>`,'');
+list.insertAdjacentHTML('beforeend', images);
+
+list.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(evt) {
+    evt.preventDefault();
+
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`)
+
+instance.show();
+
+};
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector('.gallery');
+
+
+
+const images = galleryItems.reduce((acc,{preview, original, description}) => acc +`
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`,'');
+list.insertAdjacentHTML('beforeend', images);
+
+list.addEventListener('click', onGalleryClick);
+
+
+
+function onGalleryClick(evt) {
+    evt.preventDefault();
+
+    let lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionPosition: 'bottom'});
+   
+
+
+};
